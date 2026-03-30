f26b5a898fb930fc018b658d541640b6591efb6c
Gabriel Fernandes De Sousa <95057840+GabrielFDSousa@users.noreply.github.com>
2026-03-30 20:39:42 -0300

    Initial commit

---
+++ src/main/java/com/gabriel_sousa/crud_tarefas/CrudTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel_sousa.crud_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel_sousa/crud_tarefas/CrudTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_sousa.crud_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
